3c05914e92871356128868ae968ded1ea216e5ae
mattamodio <matthewamodio@Matthews-MacBook-Pro.local>
2018-03-22 10:23:00 -0400

    initial commit

---
+++ utils.py
@@ -0,0 +1,25 @@
+import math
+import tensorflow as tf
+import numpy as np
+import struct
+
+def asinh(x, scale=5.):
+    """Asinh transform."""
+    f = np.vectorize(lambda y: math.asinh(y / scale))
+    return f(x)
+
+def sinh(x, scale=5.):
+    """Reverse transform for asinh."""
+    return scale * np.sinh(x)
+
+def lrelu(x, leak=0.2, name="lrelu"):
+    """Leaky ReLU activation."""
+    return tf.maximum(x, leak * x)
+
+def tbn(name):
+    """Get the tensor in the default graph of the given name."""
+    return tf.get_default_graph().get_tensor_by_name(name)
+
+def obn(name):
+    """Get the operation node in the default graph of the given name."""
+    return tf.get_default_graph().get_operation_by_name(name)
