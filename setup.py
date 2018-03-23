33fa3bb0fd57814dda1668fb62129c6485abf77c
mattamodio <matthewamodio@Matthews-MacBook-Pro.local>
2018-03-23 03:06:34 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-     name='pymba',    # This is the name of your PyPI-package.
-     version='0.1',                          # Update the version number for new releases
-     scripts=['utils']                  # The name of your scipt, and also the command you'll be using for calling it
+    name = 'pymba',
+    version = '0.1',
+    url = 'https://github.com/mattamodio/pymba',
+    author = 'Matt Amodio',
+    description = '',
+    packages = find_packages()
 )
