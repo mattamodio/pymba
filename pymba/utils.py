e3e72d4bd41375a8c466629a8b8a6ce047555334
mattamodio <matthewamodio@vpn172022112244.its.yale.internal>
2018-06-28 18:13:28 -0400

    Silenced stderr

--- pymba/utils.py
+++ pymba/utils.py
@@ -32,7 +32,10 @@ class Silence:
     """Suppress any printing while in context"""
     def __enter__(self):
         self._original_stdout = sys.stdout
+        self._original_stderr = sys.stderr
         sys.stdout = Silencer()
+        sys.stderr = Silencer()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout = self._original_stdout
+        sys.stderr = self._original_stderr
