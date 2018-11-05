3fb8efe54e7f67a7dbe12eb274e8a2b3ca09cf4b
mattamodio <matthewamodio@vpn172022123054.its.yale.internal>
2018-11-04 19:37:22 -0500

    blah

--- pymba/imagenet.py
+++ pymba/imagenet.py
@@ -47,23 +47,28 @@ def get_imagenet_classes():
     return classes
 
 
-def get_imagenet_data(datadir, wnid1, wnid2, D=128, tanh_transform=True):
+def get_imagenet_data(datadir, wnid1, D=128, tanh_transform=True):
     b1 = sorted(glob.glob('{}/{}/*.JPEG'.format(datadir, wnid1)))
-    b2 = sorted(glob.glob('{}/{}/*.JPEG'.format(datadir, wnid2)))
 
     b1 = [skimage.transform.imresize(plt.imread(f), (D, D)) for f in b1]
     b1 = [img for img in b1 if len(img.shape) == 3]
     b1 = np.stack(b1, axis=0)
 
-    b2 = [skimage.transform.imresize(plt.imread(f), (D, D)) for f in b2]
-    b2 = [img for img in b2 if len(img.shape) == 3]
-    b2 = np.stack(b2, axis=0)
-
     b1 = b1.astype(np.float32)
-    b2 = b2.astype(np.float32)
 
     if tanh_transform:
         b1 = (b1 / 127.5) - 1
-        b2 = (b2 / 127.5) - 1
 
-    return b1, b2
+    return b1
+
+
+
+
+
+
+
+
+
+
+
+
